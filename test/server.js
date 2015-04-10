1f6a800c68ada3215a418012d70afb4269ef0c1e
Ryan X. Charles <ryan@bitgo.com>
2015-04-10 09:50:19 -0700

    test GET /m, and switch to q-supertest

--- test/server.js
+++ test/server.js
@@ -1,4 +1,4 @@
-var request = require('supertest');
+var request = require('q-supertest');
 var should = require('chai').should();
 var server = require('../lib/server');
 
@@ -6,13 +6,11 @@ describe('server', function() {
   
   describe('GET /derive/:index', function() {
     
-    it('should return the correct xpub for the 0th index', function(done) {
-      request(server)
+    it('should return the correct xpub for the 0th index', function() {
+      return request(server)
       .get('/derive/0')
-      .end(function(err, res) {
-        if (err) return done(err);
+      .then(function(res) {
         res.body.xpub.should.equal('xpub6AfzaTPTLCnsBW1nPqW2pGweQUz8NEcWHaoQEu1jQtc35Rq8tbMQPgxo92gnYrbcGDbnaCSvyFNX1mZv2YTxroYU3ivK4WtTm9VcjADFtUM');
-        done();
       });
     });
 
@@ -20,17 +18,45 @@ describe('server', function() {
 
   describe('POST /m', function() {
     
-    it('should return a new path', function(done) {
-      request(server)
+    it('should return a new path', function() {
+      return request(server)
       .post('/m')
       .send({email: 'test@example.com'})
-      .end(function(err, res) {
+      .then(function(res) {
         should.exist(res.body.path);
         res.body.path.substr(0, 2).should.equal('m/');
         should.exist(res.body.xpub);
         res.body.xpub.substr(0, 4).should.equal('xpub');
         res.body.email.should.equal('test@example.com');
-        done();
+      });
+    });
+
+  });
+
+  describe('GET /m', function() {
+    var path;
+    var xpub;
+    var email;
+
+    before(function() {
+      return request(server)
+      .post('/m')
+      .send({email: 'test@example.com'})
+      .then(function(res) {
+        path = res.body.path;
+        xpub = res.body.xpub;
+        email = res.body.email;
+      });
+    });
+    
+    it('should the path after creating it', function() {
+      return request(server)
+      .get('/' + path)
+      .then(function(res) {
+        should.exist(res.body.xpub);
+        res.body.xpub.should.equal(xpub);
+        should.exist(res.body.email);
+        res.body.email.should.equal(email);
       });
     });
 
