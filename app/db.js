f7554d7f2a65f91bfa0ff826967719e88eddde22
Ben Chan <benchan@bitgo.com>
2015-08-23 17:51:36 -0700

    support mongolab on heroku

--- app/db.js
+++ app/db.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
+// if running in an environment with mongolab, go ahead and use it
+if (process.env['MONGOLAB_URI']) {
+ process.config.mongouri = process.env['MONGOLAB_URI'];
+}
+
 // make a new database connection and define the collections
-mongoose.connect('mongodb://localhost/' + process.config.dbname);
+mongoose.connect(process.config.mongouri);
 
 module.exports = mongoose;
